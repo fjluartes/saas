856f272d51b911fb6b39a7d96db3c1a5b02c12d7
fjgl <fjluartes@gmail.com>
2022-09-06 18:07:06 +0800

    Start 2-begin

--- book/2-begin/app/pages/csr-page.tsx
+++ book/2-begin/app/pages/csr-page.tsx
@@ -1,21 +1,18 @@
+import Button from '@material-ui/core/Button';
 import React from 'react';
 import Head from 'next/head';
 
-import Link from 'next/link';
-
-const Index = () => (
+const CSRPage = () => (
   <div>
     <Head>
-      <title>Index page</title>
+      <title>CSR page</title>
       <meta name="description" content="This is a description of the Index page" />
     </Head>
     <div style={{ padding: '0px 30px', fontSize: '15px', height: '100%', color: '#222' }}>
-      <p>Content on Index page</p>
-      <Link href='/csr-page' as='/csr-page'>
-        <a>Go to CSR page</a>
-      </Link>
+      <p>Content on CSR page</p>
+      <Button variant="outlined">Some button</Button>
     </div>
   </div>
 );
 
-export default Index;
+export default CSRPage;
