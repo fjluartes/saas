3ebc272d2e7198a9dd7b6d30df0074f8d484081f
fjgl <fjluartes@gmail.com>
2023-05-16 22:58:14 +0800

    Edits 2-begin csr-page

--- book/2-begin/app/pages/csr-page.tsx
+++ book/2-begin/app/pages/csr-page.tsx
@@ -6,9 +6,9 @@ const CSRPage = () => (
   <div>
     <Head>
       <title>CSR page</title>
-      <meta name="description" content="This is a description of the Index page" />
+      <meta name="description" content="This is a description of the CSR page" />
     </Head>
-    <div style={{ padding: '0px 30px', fontSize: '15px', height: '100%' }}>
+    <div style={{ padding: '0px 30px', fontSize: '15px', height: '100%', color: '#222' }}>
       <p>Content on CSR page</p>
       <Button variant="outlined">Some button</Button>
     </div>
